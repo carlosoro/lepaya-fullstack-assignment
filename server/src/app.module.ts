97cea8c7dd6b1bb196176ab09716ad9bcb757be8
carlos <carlosoronozc@gmail.com>
2024-06-29 19:42:38 +0200

    add database connection configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import database from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [database]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => (configService.get('database'))
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ server/src/config/database.ts
@@ -0,0 +1,23 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { registerAs } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config({ path: '.env' });
+
+const databaseConnection = {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    migrations: ["dist/migrations/*{.ts,.js}"],
+    autoLoadEntities: true,
+    synchronize: false,
+}
+
+console.log(databaseConnection);
+
+export default registerAs('database', () => databaseConnection);
+export const connectionSource = new DataSource(databaseConnection as DataSourceOptions);
