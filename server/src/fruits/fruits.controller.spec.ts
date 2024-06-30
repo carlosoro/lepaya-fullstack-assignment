898e3d69b518b4975ddd69e6238cf02d39c4e8e9
carlos <carlosoronozc@gmail.com>
2024-06-30 21:49:42 +0200

    add fruits controller tests

--- server/src/fruits/fruits.controller.spec.ts
+++ server/src/fruits/fruits.controller.spec.ts
@@ -3,9 +3,11 @@ import { FruitsController } from './fruits.controller';
 import { FruitsService } from './fruits.service';
 import { LocationsService } from './../locations/locations.service';
 import { LedgersService } from './../ledgers/ledgers.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('FruitsController', () => {
   let controller: FruitsController;
+  let fruitsService: FruitsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,9 +30,43 @@ describe('FruitsController', () => {
     }).compile();
 
     controller = module.get<FruitsController>(FruitsController);
+    fruitsService = module.get<FruitsService>(FruitsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should throw an http exception error when an error occurs while getting the report', async () => {
+    const getReportDto = { year: 2021, locationId: 1 };
+
+    jest.spyOn(fruitsService, 'getFruitReports').mockImplementation(() => {
+      throw Error('Internal server error');
+    });
+
+    expect(() => {
+      controller.getFruitReports(getReportDto)
+    }).toThrow(
+      new HttpException(
+        'An error occurred while getting the report',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      ),
+    );
+  });
+
+  it('should return valid object', async () => {
+    const getReportDto = { year: 2024, locationId: 1 };
+    const expectedResponse = {
+      mostConsumedFruit: {
+        fruitId: 1,
+        amount: 100,
+        name: 'Apple',
+      },
+      averageFruitConsumption: 10,
+    };
+    fruitsService.getFruitReports = jest.fn().mockResolvedValue(expectedResponse);
+    const response = controller.getFruitReports(getReportDto)
+    expect(response).resolves.toEqual(expectedResponse);
+  });
+
 });
--- server/src/locations/locations.service.spec.ts
+++ server/src/locations/locations.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationsService } from './locations.service';
+import { LocationsRepository } from './locations.repository';
 
 describe('LocationsService', () => {
   let service: LocationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationsService],
+      providers: [
+        LocationsService,
+        {
+          provide: LocationsRepository,
+          useValue: {
+            getLocationById: jest.fn(),
+          },
+        }
+      ],
+
     }).compile();
 
     service = module.get<LocationsService>(LocationsService);
