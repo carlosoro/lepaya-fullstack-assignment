5f11ffc5885601d81909547193268bf6f51e8e4a
carlos <carlosoronozc@gmail.com>
2024-07-02 22:23:05 +0200

    add report page basic implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Button, Card, Col, Form } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { getReport } from "./services/ledgersService";
+import { BaseSyntheticEvent, useState } from "react";
+
+interface ReportResponse {
+  data: ReportResult;
+}
+
+interface ReportResult {
+  mostConsumedFruit: MostConsumedFruit;
+  averageFruitConsumption: number;
+}
+
+interface MostConsumedFruit {
+  fruitId: string;
+  amount: number;
+  name: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const locations = [
+    { id: 1, name: 'Amsterdam', headcount: 200 },
+    { id: 2, name: 'Berlin', headcount: 100 },
+    { id: 3, name: 'Paris', headcount: 20 },
+    { id: 4, name: 'London', headcount: 50 },
+  ];
+
+  const years = [2016, 2017, 2018, 2019, 2020, 2021, 2022];
+
+  const [selectedLocation, setSelectedLocation] = useState<number>(0);
+  const [selectedYear, setSelectedYear] = useState(0);
+  const [reportResult, setReportResult] = useState<ReportResult | null>(null);
+
+  const handleLocationChange = (event: BaseSyntheticEvent) => {
+    setSelectedLocation(event.target.value);
+  }
+
+  const handleYearChange = (event: BaseSyntheticEvent) => {
+    setSelectedYear(event.target.value);
+  }
+
+  const handleSubmit = async () => {
+    const response: ReportResponse = await getReport(selectedLocation, selectedYear);
+
+    if (response?.data) {
+      setReportResult(response.data);
+    }
+  }
+
+  const getLocationName = (): string => {
+    const location = locations.find(item => item.id === Number(selectedLocation));
+    return location ? location.name : '';
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container fluid>
+      <Row>
+        <Row>
+          <Col>
+            Consumption Report
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>Location</Form.Label>
+              <Form.Select value={selectedLocation} onChange={handleLocationChange}>
+                <option value="0">Select a location</option>
+                {locations.map(location =>
+                  <option
+                    key={location.id}
+                    value={location.id}
+                  >
+                    {location.name}
+                  </option>
+                )}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" >
+              <Form.Label>Year</Form.Label>
+              <Form.Select value={selectedYear} onChange={handleYearChange}>
+                <option value="0">Select a year</option>
+                {years.map(year =>
+                  <option
+                    key={year}
+                    value={year}
+                  >
+                    {year}
+                  </option>
+                )}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" >
+              <Button variant="primary" onClick={handleSubmit}>Submit</Button>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header>Fruit Consumption Report {reportResult ? ` - Location: ${getLocationName()} | Year: ${selectedYear}` : ''}</Card.Header>
+            <Card.Body>
+              {reportResult ?
+                <>
+                  <Card.Text>Most consumed fruit: {reportResult.mostConsumedFruit.name}</Card.Text>
+                  <Card.Text>Amount consumed: {reportResult.mostConsumedFruit.amount}</Card.Text>
+                </>
+                : `Report will be displayed here.`}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
