7313582b691bd7ae0538bdea263701d400987dbc
carlos <carlosoronozc@gmail.com>
2024-07-03 17:46:27 +0200

    add form validation and error handling

--- client/src/screens/FruitPurchase.tsx
+++ client/src/screens/FruitPurchase.tsx
@@ -1,119 +1,179 @@
-import { Button, Col, Form } from "react-bootstrap";
+import { Alert, Button, Col, Form } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import { createPurchase } from "../services/ledgersService";
-import { BaseSyntheticEvent, useState } from "react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 
 function FruitPurchase() {
 
-  const locations = [
-    { id: 1, name: 'Amsterdam', headcount: 200 },
-    { id: 2, name: 'Berlin', headcount: 100 },
-    { id: 3, name: 'Paris', headcount: 20 },
-    { id: 4, name: 'London', headcount: 50 },
-  ];
+    const [selectedPurchaseLocation, setSelectedPurchaseLocation] = useState<number>(0);
+    const [selectedFruit, setSelectedFruit] = useState<number>(0);
+    const [fruitAmount, setFruitAmount] = useState<number>(0);
+    const [formSubmitDisabled, setFormSubmitDisabled] = useState<boolean>(true);
+    const [showFormError, setShowFormError] = useState<boolean>(false);
+    const [errorAlertMessage, setErrorAlertMessage] = useState<string>('');
+    const [alertState, setAlertState] = useState<{
+        show: boolean,
+        header: string,
+        message: string,
+        type: 'success' | 'danger'
+    }>({ show: false, header: 'Oh snap! You got an error!', message: '', type: 'danger' });
 
-  const fruits = [
-    { id: 1, name: 'Lime' },
-    { id: 2, name: 'Tangerine' },
-    { id: 3, name: 'Apple' },
-    { id: 4, name: 'Mango' },
-    { id: 5, name: 'Plum' },
-    { id: 6, name: 'Pineapple' },
-    { id: 7, name: 'Kiwi' },
-    { id: 8, name: 'Pear' },
-  ];
+    const locations = [
+        { id: 1, name: 'Amsterdam', headcount: 200 },
+        { id: 2, name: 'Berlin', headcount: 100 },
+        { id: 3, name: 'Paris', headcount: 20 },
+        { id: 4, name: 'London', headcount: 50 },
+    ];
 
-  const [selectedPurchaseLocation, setSelectedPurchaseLocation] = useState<number>(0);
-  const [selectedFruit, setSelectedFruit] = useState<number>(0);
-  const [fruitAmount, setFruitAmount] = useState<number>(0);
+    useEffect(() => {
+        setFormSubmitDisabled(!isFormValid());
+        setAlertState({
+            show: false,
+            header: 'Oh snap! You got an error!',
+            message: '',
+            type: 'success'
+        });
+    }, [selectedPurchaseLocation, selectedFruit, fruitAmount]);
 
-  const handlePurchaseLocationChange = (event: BaseSyntheticEvent) => {
-    setSelectedPurchaseLocation(event.target.value);
-  }
-
-  const handleFruitChange = (event: BaseSyntheticEvent) => {
-    setSelectedFruit(Number(event.target.value));
-  }
-
-  const handleFruitAmountChange = (event: BaseSyntheticEvent) => {
-    setFruitAmount(Number(event.target.value));
-  }
-
-  const handlePurchaseSubmit = async () => {
-    const response = await createPurchase({
-      locationId: selectedPurchaseLocation,
-      fruitId: selectedFruit,
-      amount: fruitAmount
-    });
-
-    if (response) {
-      console.log('Purchase created successfully', response);
+    const isFormValid = (): boolean => {
+        return selectedPurchaseLocation !== 0
+            && selectedFruit !== 0
+            && fruitAmount > 0;
     }
-  }
 
-  return (
-    <Container fluid>
-      <Row>
-        <Row>
-          <Col>
-            Create Purchase
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label>Location</Form.Label>
-              <Form.Select value={selectedPurchaseLocation} onChange={handlePurchaseLocationChange}>
-                <option value="0">Select a location</option>
-                {locations.map(location =>
-                  <option
-                    key={location.id + 21}
-                    value={location.id}
-                  >
-                    {location.name}
-                  </option>
-                )}
-              </Form.Select>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label>Fruit</Form.Label>
-              <Form.Select value={selectedFruit} onChange={handleFruitChange}>
-                <option value="0">Select a fruit</option>
-                {fruits.map(fruit =>
-                  <option
-                    key={fruit.id + 100}
-                    value={fruit.id}
-                  >
-                    {fruit.name}
-                  </option>
-                )}
-              </Form.Select>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label>Amount</Form.Label>
-              <Form.Control type="number" value={fruitAmount} onChange={handleFruitAmountChange} />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3" >
-              <Button variant="primary" onClick={handlePurchaseSubmit}>Submit</Button>
-            </Form.Group>
-          </Col>
-        </Row>
-      </Row>
-    </Container>
-  )
+    const fruits = [
+        { id: 1, name: 'Lime' },
+        { id: 2, name: 'Tangerine' },
+        { id: 3, name: 'Apple' },
+        { id: 4, name: 'Mango' },
+        { id: 5, name: 'Plum' },
+        { id: 6, name: 'Pineapple' },
+        { id: 7, name: 'Kiwi' },
+        { id: 8, name: 'Pear' },
+    ];
+
+    const handlePurchaseLocationChange = (event: BaseSyntheticEvent) => {
+        setSelectedPurchaseLocation(event.target.value);
+    }
+
+    const handleFruitChange = (event: BaseSyntheticEvent) => {
+        setSelectedFruit(Number(event.target.value));
+    }
+
+    const handleFruitAmountChange = (event: BaseSyntheticEvent) => {
+        const amount = parseFloat(event.target.value) || 0;
+        event.target.value = amount;
+        setFruitAmount(amount);
+    }
+
+    const handleSubmit = async () => {
+        try {
+            if (!isFormValid()) {
+                setAlertState({
+                    show: true,
+                    header: 'Oh snap! You got an error!',
+                    message: 'Make sure you selected filled all options and try again.',
+                    type: 'danger'
+                });
+            }
+            const response = await createPurchase({
+                locationId: selectedPurchaseLocation,
+                fruitId: selectedFruit,
+                amount: fruitAmount
+            });
+
+            if (response) {
+                setAlertState({
+                    show: true,
+                    header: 'Great Success!',
+                    message: 'Purchase created.',
+                    type: 'success'
+                });
+            }
+        } catch (error) {
+            const message = (error as Error)?.message || 'An error occurred while generating the report.';
+            setAlertState({ show: true, header: 'Oh snap! You got an error!', message, type: 'danger' });
+        }
+    }
+
+    return (
+        <Container fluid>
+            <Row>
+                <Row>
+                    <Col>
+                        Create Purchase
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {alertState.show &&
+                            <Alert variant={alertState.type} onClose={() => setShowFormError(false)} dismissible>
+                                <Alert.Heading>{alertState.header}</Alert.Heading>
+                                <p>
+                                    {(alertState.message) ?
+                                        alertState.message :
+                                        `Make sure you selected filled all options and try again.`
+                                    }
+                                </p>
+                            </Alert>
+                        }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Location</Form.Label>
+                            <Form.Select value={selectedPurchaseLocation} onChange={handlePurchaseLocationChange}>
+                                <option value="0">Select a location</option>
+                                {locations.map(location =>
+                                    <option
+                                        key={location.id + 21}
+                                        value={location.id}
+                                    >
+                                        {location.name}
+                                    </option>
+                                )}
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Fruit</Form.Label>
+                            <Form.Select value={selectedFruit} onChange={handleFruitChange}>
+                                <option value="0">Select a fruit</option>
+                                {fruits.map(fruit =>
+                                    <option
+                                        key={fruit.id + 100}
+                                        value={fruit.id}
+                                    >
+                                        {fruit.name}
+                                    </option>
+                                )}
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Amount</Form.Label>
+                            <Form.Control type="number" value={fruitAmount} onChange={handleFruitAmountChange} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Button variant="primary" disabled={formSubmitDisabled ? true : false} onClick={handleSubmit}>Submit</Button>
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Row>
+        </Container>
+    )
 }
 
 export default FruitPurchase;
--- client/src/screens/Report.tsx
+++ client/src/screens/Report.tsx
@@ -1,117 +1,153 @@
-import { Button, Card, Col, Form } from "react-bootstrap";
+import { Alert, Button, Card, Col, Form } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import { getReport } from "../services/ledgersService";
-import { BaseSyntheticEvent, useState } from "react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 import { FruitReportResponse } from "../types";
 
 function Report() {
+    const [selectedLocation, setSelectedLocation] = useState<number>(0);
+    const [selectedYear, setSelectedYear] = useState<number>(0);
+    const [reportResult, setReportResult] = useState<FruitReportResponse | null>(null);
+    const [formSubmitDisabled, setFormSubmitDisabled] = useState<boolean>(true);
+    const [showFormError, setShowFormError] = useState<boolean>(false);
+    const [errorAlertMessage, setErrorAlertMessage] = useState<string>('');
 
-  const locations = [
-    { id: 1, name: 'Amsterdam', headcount: 200 },
-    { id: 2, name: 'Berlin', headcount: 100 },
-    { id: 3, name: 'Paris', headcount: 20 },
-    { id: 4, name: 'London', headcount: 50 },
-  ];
+    useEffect(() => {
+        setFormSubmitDisabled(!isFormValid());
+    }, [selectedLocation, selectedYear]);
 
-  const years = [2016, 2017, 2018, 2019, 2020, 2021, 2022];
+    const locations = [
+        { id: 1, name: 'Amsterdam', headcount: 200 },
+        { id: 2, name: 'Berlin', headcount: 100 },
+        { id: 3, name: 'Paris', headcount: 20 },
+        { id: 4, name: 'London', headcount: 50 },
+    ];
 
-  const [selectedLocation, setSelectedLocation] = useState<number>(0);
-  const [selectedYear, setSelectedYear] = useState<number>(0);
-  const [reportResult, setReportResult] = useState<FruitReportResponse | null>(null);
+    const years = [2016, 2017, 2018, 2019, 2020, 2021, 2022];
 
-  const handleLocationChange = (event: BaseSyntheticEvent) => {
-    setSelectedLocation(event.target.value);
-  }
 
-  const handleYearChange = (event: BaseSyntheticEvent) => {
-    setSelectedYear(event.target.value);
-  }
+    const handleLocationChange = (event: BaseSyntheticEvent) => {
+        setSelectedLocation(Number(event.target.value));
+        setErrorAlertMessage('');
+        setShowFormError(false);
+    }
 
-  const handleSubmit = async () => {
-    try {
-      const response = await getReport(selectedLocation, selectedYear);
+    const handleYearChange = (event: BaseSyntheticEvent) => {
+        setSelectedYear(Number(event.target.value));
+        setErrorAlertMessage('');
+        setShowFormError(false);
+    }
 
-      if (response) {
-        setReportResult(response);
-      }
-    } catch (error) { }
-  }
+    const handleSubmit = async () => {
+        try {
+            if (!isFormValid()) {
+                setShowFormError(true);
+            }
+            const response = await getReport(selectedLocation, selectedYear);
+            if (response) {
+                setReportResult(response);
+            }
+        } catch (error) {
+            const message = (error as Error)?.message || 'An error occurred while generating the report.';
+            setErrorAlertMessage(message);
+            setShowFormError(true);
+        }
+    }
 
-  const getLocationName = (): string => {
-    const location = locations.find(item => item.id === Number(selectedLocation));
-    return location ? location.name : '';
-  }
+    const isFormValid = (): boolean => {
+        return selectedLocation !== 0 && selectedYear !== 0;
+    }
 
-  return (
-    <Container fluid>
-      <Row>
-        <Row>
-          <Col>
-            Consumption Report
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label>Location</Form.Label>
-              <Form.Select value={selectedLocation} onChange={handleLocationChange}>
-                <option value="0">Select a location</option>
-                {locations.map(location =>
-                  <option
-                    key={location.id}
-                    value={location.id}
-                  >
-                    {location.name}
-                  </option>
-                )}
-              </Form.Select>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3" >
-              <Form.Label>Year</Form.Label>
-              <Form.Select value={selectedYear} onChange={handleYearChange}>
-                <option value="0">Select a year</option>
-                {years.map(year =>
-                  <option
-                    key={year}
-                    value={year}
-                  >
-                    {year}
-                  </option>
-                )}
-              </Form.Select>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Group className="mb-3" >
-              <Button variant="primary" onClick={handleSubmit}>Submit</Button>
-            </Form.Group>
-          </Col>
-        </Row>
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Header>Fruit Consumption Report {reportResult ? ` - Location: ${getLocationName()} | Year: ${selectedYear}` : ''}</Card.Header>
-            <Card.Body>
-              {reportResult ?
-                <>
-                  <Card.Text>Most consumed fruit: {reportResult.mostConsumedFruit.name}</Card.Text>
-                  <Card.Text>Amount consumed: {reportResult.mostConsumedFruit.amount}</Card.Text>
-                </>
-                : `Report will be displayed here.`}
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  )
+    const getLocationName = (): string => {
+        const location = locations.find(item => item.id === Number(selectedLocation));
+        return location ? location.name : '';
+    }
+
+    return (
+        <Container fluid>
+            <Row>
+                <Row>
+                    <Col>
+                        Consumption Report
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {showFormError &&
+                            <Alert variant="danger" onClose={() => setShowFormError(false)} dismissible>
+                                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                                <p>
+                                    {(errorAlertMessage) ?
+                                        errorAlertMessage :
+                                        `Make sure you selected both location and year and try again.`
+                                    }
+                                </p>
+                            </Alert>
+                        }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Location</Form.Label>
+                            <Form.Select value={selectedLocation} onChange={handleLocationChange}>
+                                <option value="0">Select a location</option>
+                                {locations.map(location =>
+                                    <option
+                                        key={location.id}
+                                        value={location.id}
+                                    >
+                                        {location.name}
+                                    </option>
+                                )}
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Year</Form.Label>
+                            <Form.Select value={selectedYear} onChange={handleYearChange}>
+                                <option value="0">Select a year</option>
+                                {years.map(year =>
+                                    <option
+                                        key={year}
+                                        value={year}
+                                    >
+                                        {year}
+                                    </option>
+                                )}
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Button variant="primary" disabled={formSubmitDisabled ? true : false} onClick={handleSubmit}>Submit</Button>
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Header>Fruit Consumption Report {reportResult ? ` - Location: ${getLocationName()} | Year: ${selectedYear}` : ''}</Card.Header>
+                        <Card.Body>
+                            {reportResult ?
+                                <>
+                                    <Card.Text>Most consumed fruit: {reportResult.mostConsumedFruit.name}</Card.Text>
+                                    <Card.Text>Amount consumed: {reportResult.mostConsumedFruit.amount}</Card.Text>
+                                </>
+                                : `Report will be displayed here.`}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default Report;
