620776da4ff038bcf6c04c3956238556119d80c5
carlos <carlosoronozc@gmail.com>
2024-07-04 12:02:49 +0200

    add server locations service tests

--- server/src/modules/locations/locations.service.spec.ts
+++ server/src/modules/locations/locations.service.spec.ts
@@ -4,6 +4,7 @@ import { LocationsRepository } from './locations.repository';
 
 describe('LocationsService', () => {
   let service: LocationsService;
+  let locationsRepository: LocationsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,9 +21,23 @@ describe('LocationsService', () => {
     }).compile();
 
     service = module.get<LocationsService>(LocationsService);
+    locationsRepository = module.get<LocationsRepository>(LocationsRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getLocationById', () => {
+    it('should return a location', async () => {
+      const LocationId = 1;
+      const locationObject = {
+        id: 1,
+        name: 'Amsterdam',
+      }
+      locationsRepository.getById = jest.fn().mockResolvedValue(locationObject);
+      const result = await service.getLocationById(LocationId);
+      expect(result).toEqual(locationObject);
+    });
   });
 });
