0baed8ac0da4cacd3d5eb7e639151ef51a557b15
carlos <carlosoronozc@gmail.com>
2024-07-05 12:26:35 +0200

    add react router link implementations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,57 @@
-import { Route, Routes } from "react-router-dom";
-import Report from "./screens/Report";
-import FruitPurchase from "./screens/FruitPurchase";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { routes } from "./routes";
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { routes } from './routes';
 
 function App() {
-
   return (
     <>
-      <Container>
-        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container className='d-grid gap-5'>
+        <Navbar expand='lg' className='bg-body-tertiary'>
           <Container>
-            <Navbar.Brand href="/"><h1>Chocolate Inc</h1></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/">Reports</Nav.Link>
-                <Nav.Link href="/purchases">Purchases</Nav.Link>
+            <Navbar.Brand>
+              <NavLink to='/' className='text-decoration-none'>
+                <h1>Chocolate Inc</h1>
+              </NavLink>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto position-absolute end-0'>
+                <Nav.Link>
+                  <NavLink
+                    to='/'
+                    className={({ isActive }) =>
+                      isActive
+                        ? 'fw-bold text-decoration-none'
+                        : 'text-muted text-decoration-none'
+                    }
+                  >
+                    Reports
+                  </NavLink>
+                </Nav.Link>
+                <Nav.Link>
+                  <NavLink
+                    to='/purchases'
+                    className={({ isActive }) =>
+                      isActive
+                        ? 'fw-bold text-decoration-none'
+                        : 'text-muted text-decoration-none'
+                    }
+                  >
+                    Purchases
+                  </NavLink>
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
         <Routes>
-          {routes.map((route, index) =>
+          {routes.map((route, index) => (
             <Route key={index} path={route.path} element={route.element} />
-          )}
+          ))}
         </Routes>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
